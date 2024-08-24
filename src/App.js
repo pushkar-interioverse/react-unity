1ef57dd0e132597e2701297e75df006cabfcc468
pushkar-interioverse <thaturpushkar99@gmail.com>
2024-08-24 11:39:50 +0530

    Making minor changes

--- src/App.js
+++ src/App.js
@@ -24,10 +24,20 @@ const App = () => {
   // };
 
   const openUnity3App = () => {
-    // Construct the Unity WebGL URL for the new project
-    const unity2Url = `${window.location.origin}/unity3/index.html`;
-    window.open(unity2Url, "_blank");
+    const jsonData = {
+      name: "Pushkar",
+      accessToken: "qwdwkjfgikvjeflinlkdcwoicnw89dkjcsjdc-djvcldyvidcjd",
+      refreshToken: "skdbveiyrhv'ew0id-]e=osdv jhbv fdcmslkdcijc0-ew",
+    };
+  
+    // Encode JSON data to Base64 for passing via query parameter
+    const encodedData = btoa(JSON.stringify(jsonData));
+  
+    // Construct the Unity WebGL URL with encoded JSON data
+    const unityUrl = `${window.location.origin}/unity3/index.html?jsonData=${encodedData}`;
+    window.open(unityUrl, "_blank");
   };
+  
 
   return (
     <div>
