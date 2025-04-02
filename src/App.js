65db9685ae8994a3ddb5a009a0c2bab65005e5f1
pushkar-interioverse <pushkar.t@interioverse.com>
2025-04-02 11:19:10 +0530

    Add Git LFS tracking for Unity build files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React from "react";
 
 const App = () => {
-  const openUnityApp = () => {
-    const jsonData = {
-      name: "Pushkar",
-      accessToken: "qwdwkjfgikvjeflinlkdcwoicnw89dkjcsjdc-djvcldyvidcjd",
-      refreshToken: "skdbveiyrhv'ew0id-]e=osdv jhbv fdcmslkdcijc0-ew",
-    };
-
-    // Encode JSON data to Base64 for passing via query parameter
-    const encodedData = btoa(JSON.stringify(jsonData));
-
-    // Construct the Unity WebGL URL with encoded JSON data
-    const unityUrl = `${window.location.origin}/unity?jsonData=${encodedData}`;
-    window.open(unityUrl, "_blank");
-  };
 
 
   // const openUnityApp = () => {
@@ -38,19 +24,18 @@ const App = () => {
     window.open(unityUrl, "_blank");
   };
 
-  const openUnity3App = () => {
-    // Construct the Unity WebGL URL for the new project
-    const unity2Url = `${window.location.origin}/unity3/index.html`;
-    window.open(unity2Url, "_blank");
-  };
-
   return (
     <div>
       <h1>Unity WebGL in React</h1>
-      <button onClick={openUnityApp}>Open Unity App</button>
-      <button onClick={openUnity3App}>Open Unity3 App</button>
       <button onClick={openUnity2App}>Open Unity2 App</button>
+      <button
+        className="unity-explore-design-button"
+        onClick={() => window.open(`${window.location.origin}/unity3/index.html`, "_blank")}
+      >
+        Explore Design Ideas
+      </button>
     </div>
+    
   );
 };
 
